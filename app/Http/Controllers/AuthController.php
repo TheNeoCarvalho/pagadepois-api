b7753d6254461a637acd6c4635fbfa7623223ac8
Manoel Carvalho <manoelmecat@gmail.com>
2026-04-30 08:06:43 -0300

    feat: request e register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -32,13 +32,33 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $validated = $request->validate([
-            'name' => 'required|string|max:255',
+            'name' => 'required|string|max:100|min:3',
             'email' => 'required|string|email|unique:users',
             'password' => 'required|string|min:8',
         ]);
 
-        $token = $user->create
+        $userExist = User::where('email', $validated['email'])->first();
 
+        if($userExist){
+            return response()->json([
+                'message' => 'O email já está cadastrado, use outro email!'
+            ], 400);
+        }
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+        
+        $token = $user->createToken('auth_token')->plainTextToken();
+        
+        return response()->json([
+            'message' => 'Usuário criado com sucesso!',
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+            ], 201);
+            
     }
 
     /**
