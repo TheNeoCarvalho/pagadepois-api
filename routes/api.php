be7f2ec25a9b8a9577d1e9c7ea18aff911bbeda2
Manoel Carvalho <manoelmecat@gmail.com>
2026-05-22 12:49:40 -0300

    fix: routes

--- routes/api.php
+++ routes/api.php
@@ -11,4 +11,5 @@ Route::prefix('auth')->group(function () {
         Route::get('/me', [AuthController::class, 'me']);
         Route::post('/logout', [AuthController::class, 'logout']);
     });
+    
 });
